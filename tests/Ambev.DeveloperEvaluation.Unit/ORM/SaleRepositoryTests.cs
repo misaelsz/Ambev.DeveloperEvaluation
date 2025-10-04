6d4c9353ee45495c5bc88c414050f8274fd91258
Misael Zeferino <misaelzeferino@outlook.com>
2025-10-04 09:47:34 -0300

    Unit Tests for sale repository GetBySaleNumberAsync

--- tests/Ambev.DeveloperEvaluation.Unit/ORM/SaleRepositoryTests.cs
+++ tests/Ambev.DeveloperEvaluation.Unit/ORM/SaleRepositoryTests.cs
@@ -51,4 +51,26 @@ public class SaleRepositoryTests
         result.Status.Should().Be(sale.Status);
         await saleRepository.Received(1).GetByIdAsync(Arg.Any<Guid>(), Arg.Any<CancellationToken>());
     }
+
+    [Fact(DisplayName = "Given valid sale number When getting sale Then returns found sale")]
+    public async Task GetBySaleNumberAsync_ValidSaleNumber_ReturnsFoundSale()
+    {
+        var saleRepository = Substitute.For<ISaleRepository>();
+        var sale = SaleRepositoryTestData.GenerateValidSale();
+        var saleNumber = sale.SaleNumber;
+
+        saleRepository.GetBySaleNumberAsync(Arg.Any<string>(), Arg.Any<CancellationToken>())
+            .Returns(sale);
+
+        var result = await saleRepository.GetBySaleNumberAsync(saleNumber, CancellationToken.None);
+
+        result.Should().NotBeNull();
+        result!.SaleNumber.Should().Be(saleNumber);
+        result.Id.Should().Be(sale.Id);
+        result.CustomerName.Should().Be(sale.CustomerName);
+        result.BranchName.Should().Be(sale.BranchName);
+        result.TotalAmount.Should().Be(sale.TotalAmount);
+        result.Status.Should().Be(sale.Status);
+        await saleRepository.Received(1).GetBySaleNumberAsync(Arg.Any<string>(), Arg.Any<CancellationToken>());
+    }
 }
