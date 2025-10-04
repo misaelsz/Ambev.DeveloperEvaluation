5e4734ef45763739c4e205cc497967008b4552f7
Misael Zeferino <misaelzeferino@outlook.com>
2025-10-04 14:19:22 -0300

    Unit tests for BranchRepository CreateAsync

---
+++ tests/Ambev.DeveloperEvaluation.Unit/ORM/TestData/BranchRepositoryTestData.cs
@@ -0,0 +1,26 @@
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+using Bogus;
+
+namespace Ambev.DeveloperEvaluation.Unit.ORM.TestData;
+
+public static class BranchRepositoryTestData
+{
+    private static readonly Faker<Branch> branchFaker = new Faker<Branch>()
+        .RuleFor(b => b.Name, f => f.Company.CompanyName())
+        .RuleFor(b => b.Code, f => f.Random.AlphaNumeric(6).ToUpper())
+        .RuleFor(b => b.Address, f => f.Address.StreetAddress())
+        .RuleFor(b => b.City, f => f.Address.City())
+        .RuleFor(b => b.State, f => f.Address.StateAbbr())
+        .RuleFor(b => b.PostalCode, f => f.Address.ZipCode())
+        .RuleFor(b => b.Phone, f => f.Phone.PhoneNumber("(##) ####-####"))
+        .RuleFor(b => b.ManagerName, f => f.Person.FullName)
+        .RuleFor(b => b.Status, f => f.PickRandom<BranchStatus>())
+        .RuleFor(b => b.CreatedAt, f => f.Date.Recent())
+        .RuleFor(b => b.UpdatedAt, f => f.Date.Recent());
+
+    public static Branch GenerateValidBranch()
+    {
+        return branchFaker.Generate();
+    }
+}
